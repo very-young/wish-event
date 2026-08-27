4b9a31dc2cbd2ea4fb60508e7b03a25a07dcc266
very-young <kkso990310@gmail.com>
2026-08-27 11:44:29 +0900

    fix: 성공 편지에서 선물 언급 제거

--- src/components/screens/LetterScreen.tsx
+++ src/components/screens/LetterScreen.tsx
@@ -104,15 +104,7 @@ export function LetterScreen({
             </button>
 
             <p className="lead" style={{ textAlign: "center" }}>
-              {/*
-                경품을 실제로 받는 사람에게만 선물을 언급한다.
-                소진 시에는 성공했어도 줄 선물이 없다.
-              */}
-              {(prizeWon
-                ? LETTER.arrivedWin
-                : won
-                  ? LETTER.arrivedSuccess
-                  : LETTER.arrivedLose)
+              {(won ? LETTER.arrivedWin : LETTER.arrivedLose)
                 .split("\n")
                 .map((line, i) => (
                   <span key={i}>
